679a732dad8c3175e8bb36ced88a3744170ec175
Nabeel Valley <nabeelvalley1@hotmail.com>
2018-12-05 12:08:22 +0200

    add summary builder script

---
+++ make_summary.ps1
@@ -0,0 +1,26 @@
+$readme_title = "Nabeel's Docs"
+$asset_folder = ".gitbook"
+
+$summary = "# Table of Contents`n`n* [$readme_title](README.md)`n`n"
+
+Get-ChildItem -Directory | ForEach-Object {
+	$directory = $_.Name
+	if ($directory-ne $asset_folder) {
+		$print_dir = ""
+		$dir_words = $directory.split("-")
+		foreach ($word in $dir_words) {
+			$print_dir += $word.substring(0, 1).toupper() + $word.substring(1) + " "
+		}
+		$summary += "## $print_dir`n`n"
+		Set-Location $directory;
+		Get-ChildItem -File | ForEach-Object {
+			$file = $_.Name
+			$line = (Get-Content $file -First 1).Replace("# ", "")
+			$summary += "* [$line]($directory/$file)`n"
+		}
+		$summary += "`n"
+		Set-Location ..
+	}
+}
+
+$summary | Out-File "SUMMARY.md"
