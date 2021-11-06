15fe69b853aabb82e47cb0ecf452c154bc94ac2d
Nabeel Valley <nabeelvalley@outlook.com>
2021-11-06 12:29:05 +0200

    :sparkles: parsing of ipynb to output pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,42 @@
+const ipynb = require('ipynb2html')
+const jsdom = require('jsdom')
+
+const transformIpynb = require('./lib/transform-ipynb')
+
 module.exports = function (config) {
-  config.addPassthroughCopy("assets")
+  config.addPassthroughCopy('assets')
+  // config.addPassthroughCopy('content/*/*/*.ipynb')
+
+  // config.addTemplateFormats('ipynb')
+  config.addExtension('ipynb', {
+    compile: (str, inputPath) => (data) => {
+      return 'Hello World'
+
+      const window = new jsdom.JSDOM().window
+      const document = window.document
+
+      const renderNotebook = ipynb.createRenderer(document)
+      const notebook = JSON.parse(str)
+
+      const renderedContent = renderNotebook(notebook)
+
+      renderedContent
+        .querySelectorAll('style')
+        .forEach((el) => el.parentNode.removeChild(el))
+
+      const html = renderedContent.innerHTML
+        .replace(/class="dataframe"/g, '')
+        .replace(/border="1"/g, '')
+
+      return html
+    },
+  })
+
+  // config.addTransform('jupyterNotebook', transformIpynb)
 
   return {
     dir: {
-      input: "content",
+      input: 'content',
     },
   }
 }
--- content/_data/pages.js
+++ content/_data/pages.js
@@ -3,6 +3,9 @@ const glob = require('glob')
 const { resolve } = require('path')
 const _ = require('lodash')
 
+const ipynb = require('ipynb2html')
+const jsdom = require('jsdom')
+
 const getFiles = (ext) => {
   const g = `content/**/*.${ext}`
 
@@ -11,34 +14,76 @@ const getFiles = (ext) => {
       if (err) rej(err)
       else {
         const extRx = new RegExp(`\.${ext}$`)
-        const result = matches.map((m) =>
-          m.replace('content', '').replace(extRx, '')
-        )
+        const result = matches.map((m) => ({
+          path: m,
+          route: m.replace('content', '').replace(extRx, ''),
+        }))
         res(result)
       }
     })
   })
 }
 
-const readMeta = async (path) => {
-  const fullPath = resolve(__dirname, '../', './' + path + '.json')
+const readMeta = async (data) => {
+  const fullPath = resolve(__dirname, '../', './' + data.route + '.json')
   const content = await readFile(fullPath)
 
   const meta = JSON.parse(content.toString())
-  return { ...meta, path }
+  return { ...meta, ...data }
+}
+
+const readNotebook = async (path) => {
+  const fullPath = resolve(__dirname, '../../', path)
+  const content = await readFile(fullPath)
+
+  const window = new jsdom.JSDOM().window
+  const document = window.document
+
+  const renderNotebook = ipynb.createRenderer(document)
+  const notebook = JSON.parse(content)
+
+  const renderedContent = renderNotebook(notebook)
+
+  renderedContent
+    .querySelectorAll('style')
+    .forEach((el) => el.parentNode.removeChild(el))
+
+  const html = renderedContent.innerHTML
+    .replace(/class="dataframe"/g, '')
+    .replace(/border="1"/g, '')
+
+  return html
 }
 
 module.exports = async function () {
   const md = await getFiles('md')
+  const ipynb = await getFiles('ipynb')
+
+  const content = [...md, ...ipynb]
+
   const json = await getFiles('json')
 
-  const pages = _.intersection(md, json)
+  const pages = content.filter((c) => json.find((j) => j.route === c.route))
 
   const meta = await Promise.all(pages.map(readMeta))
 
-  const docs = meta.filter((p) => p.path.startsWith('/docs'))
-  const stdout = meta.filter((p) => p.path.startsWith('/stdout'))
-  const blog = meta.filter((p) => p.path.startsWith('/blog'))
+  const docs = meta.filter((m) => m.route.startsWith('/docs'))
+  const stdout = meta.filter((m) => m.route.startsWith('/stdout'))
+  const blog = meta.filter((m) => m.route.startsWith('/blog'))
+  const nbPromises = meta
+    .filter((m) => m.path.endsWith('.ipynb'))
+    .map(async (m) => {
+      const html = await readNotebook(m.path)
 
-  return { pages, meta, docs, stdout, blog }
+      return {
+        ...m,
+        html,
+      }
+    })
+
+  const notebooks = await Promise.all(nbPromises)
+
+  console.log(docs)
+
+  return { pages, meta, docs, stdout, blog, notebooks }
 }
