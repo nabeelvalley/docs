d43a057f2f32b2e995c280fe3d9b6e314eef633f
Nabeel Valley <nabeelvalley@outlook.com>
2021-11-18 21:46:18 +0200

    :sparkles: outputs searchable index file

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
   eleventyConfig.setLibrary('md', { render: convertMarkdownToHtml })
 
+  eleventyConfig.addCollection('search', (collection) => {
+    const result = collection.getAll().map((c) => {
+      return {
+        title: c.data && c.data.title,
+        subtitle: c.data && c.data.subtitle,
+        content: c.template && c.template.inputContent,
+        url: c.url,
+      }
+    })
+
+    return result
+  })
+
   return {
     dir: {
       input: 'content',
