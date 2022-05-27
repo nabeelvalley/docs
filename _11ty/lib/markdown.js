ae01ef7b42fdd3ce759afb2cbb9493fefc071708
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2022-05-27 18:28:45 +0200

    :lipstick: omg default html tables are gross

--- _11ty/lib/markdown.js
+++ _11ty/lib/markdown.js
@@ -1,15 +1,31 @@
 const markdownIt = require('markdown-it')
 const markdownItAnchor = require('markdown-it-anchor')
+const markdownItHljs = require('markdown-it-highlightjs')
+const markdownItKatex = require('markdown-it-katex')
 
 const jsdom = require('jsdom')
 const ipynb = require('ipynb2html')
 
-const markdownLibrary = markdownIt({
+const lib = markdownIt({
   html: true,
-}).use(markdownItAnchor.default)
+})
+  .use(markdownItAnchor.default)
+  .use(markdownItHljs)
+  .use(markdownItKatex)
+  .use((md) => {
+    md.inline.State
+  })
 
 const convertMarkdownToHtml = (markdown) => {
-  return markdownLibrary.render(markdown)
+  const base = lib.render(markdown)
+
+  return base
+    .replace(/<table /g, '<div class="scrollable"><table ')
+    .replace(/<\/table> /g, '</table><div>')
+}
+
+const markdownLibrary = {
+  render: convertMarkdownToHtml,
 }
 
 const convertJupyterToHtml = (content) => {
