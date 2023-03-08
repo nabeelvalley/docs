d2aba3b230e731273b84c52ade27249b1e6371fc
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-03-08 18:57:17 +0200

    :mag: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react'
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '_site',
-  integrations: [react()]
-});
+  site: 'https://nabeelvalley.co.za',
+  integrations: [react(), sitemap()],
+})
