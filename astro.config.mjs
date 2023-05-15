097591e0970a813290209c776d6f2d929436cd80
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-05-15 22:21:30 +0200

    :lipstick: update code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,19 @@ import mdx from '@astrojs/mdx'
 export default defineConfig({
   outDir: '_site',
   site: 'https://nabeelvalley.co.za/',
+
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula-soft',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
   integrations: [react(), sitemap(), mdx()],
 })
