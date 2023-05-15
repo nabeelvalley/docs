49a6a2ac78fff5ae2443ca214227dfd99aa43a13
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-05-15 22:51:45 +0200

    :wrench: disable astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 
-import mdx from '@astrojs/mdx'
+// import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,5 +23,5 @@ export default defineConfig({
       wrap: true,
     },
   },
-  integrations: [react(), sitemap(), mdx()],
+  integrations: [react(), sitemap()],
 })
