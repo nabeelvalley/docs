2181dc93c836d52bce5e7347aba5925d6f507da9
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-05-20 09:56:22 +0000

    :wrench: add astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // import mdx from '@astrojs/mdx'
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '_site',
   site: 'https://nabeelvalley.co.za/',
-
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -17,10 +18,11 @@ export default defineConfig({
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
+      langs: []
       // Enable word wrap to prevent horizontal scrolling
       // wrap: true,
-    },
+    }
   },
-  integrations: [react(), sitemap()],
-})
+
+  integrations: [react(), sitemap(), mdx()]
+});
