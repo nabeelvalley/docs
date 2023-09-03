cbed463873df417a82f59fa7391f9a408526d50a
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-09-03 23:06:53 +0200

    :sparkles: notebooks render

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -6,6 +6,14 @@ import markdownItToc from 'markdown-it-table-of-contents'
 
 import { JSDOM } from 'jsdom'
 import { createRenderer } from 'ipynb2html'
+import { promises } from 'fs'
+
+
+import sanitize from 'sanitize-html'
+import { readMeta } from './meta'
+
+const { writeFile, mkdir, rm, readFile } = promises
+
 
 const lib = markdownIt({
   html: true,
@@ -31,23 +39,93 @@ export const markdownLibrary = {
   render: convertMarkdownToHtml,
 }
 
-export const convertJupyterToHtml = (content) => {
-  const window = new JSDOM().window
-  const document = window.document
 
-  const renderNotebook = createRenderer(document)
+const toHeader = (data) => {
+  const lines = ['---', 'published: true']
+  const keys = ['title', 'subtitle', 'description', 'date']
 
-  const notebook = JSON.parse(content)
+  keys.forEach((key) => {
+    if (data[key]) {
+      lines.push(`${key}: ${data[key]}`)
+    }
+  })
 
-  // remove all inline style tags
-  let renderedContent = renderNotebook(notebook)
-  renderedContent
-    .querySelectorAll('style')
-    .forEach((el) => el.parentNode.removeChild(el))
+  lines.push('---')
 
-  const html = renderedContent.innerHTML
-    .replace(/class="dataframe"/g, '')
-    .replace(/border="1"/g, '')
+  return `${lines.join('\n')}`
+}
+
+const clean = (html) =>
+  sanitize(html || '', {
+    allowedTags: sanitize.defaults.allowedTags.concat(['img']),
+    allowedAttributes: {
+      img: ['src', 'alt'],
+    },
+    allowedSchemes: ['data', 'http', 'https'],
+  })
+
+const nbImageExt = '.nb-out.png'
+
+export const renderJupyterNotebook = async (slug) => {
+  const file = await readFile(`${slug}.ipynb`)
+  const ipynb = JSON.parse(file.toString())
+
+  const markdownTasks = ipynb.cells.map(async (cell, cellInd) => {
+    const type = cell.cell_type
+
+    if (type === 'markdown' && cell.source.join) {
+      return cell.source.join('')
+    }
+
+    if (type === 'code') {
+      const code = cell.source.join
+        ? '```py\n' + cell.source.join('') + '\n```\n'
+        : ''
+
+      const outputTasks = cell.outputs.map(async (out, outInd) => {
+        if (!out.data) {
+          return ''
+        }
+
+        const data = out.data
+
+        let content = ''
+
+        if (data['text/html']) {
+          content += clean(
+            data['text/html'].join
+              ? data['text/html'].join('')
+              : data['text/html']
+          )
+        } else if (data['text/plain']) {
+          content += '\n```\n' + data['text/plain'] + '\n```\n'
+        }
+
+        if (data['image/png']) {
+          const filePath = `${slug.replaceAll(
+            '/',
+            '__'
+          )}-${cellInd}-${outInd}${nbImageExt}`
+
+          await writeFile(`public/${filePath}`, data['image/png'], 'base64')
+
+          content += `<img src="/${filePath}" />`
+        }
+
+        return content
+      })
+
+      const output = await Promise.all(outputTasks)
+
+      return code + output.join('\n\n')
+    }
+  })
+
+  const markdown = (await Promise.all(markdownTasks)).join('\n\n')
+
+
+  const html = convertMarkdownToHtml(markdown)
 
   return html
 }
+
