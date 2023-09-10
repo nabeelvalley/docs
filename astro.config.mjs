c017f477c9d7d50572ca9f6e7120b4767cd33ba7
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-09-10 22:54:25 +0200

    :sparkles: using new layouts for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'dracula-soft',
+      theme: 'min-light',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
