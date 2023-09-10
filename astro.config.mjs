9b8a1119a12c6d566219ddd6693a277546172136
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2023-09-10 23:09:56 +0200

    :lipstick: :iphone: responsiveness and styling updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'min-light',
+      theme: 'min-dark',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
