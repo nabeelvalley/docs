2156e3981d7a8b40cb9df49fe69de77441a6c712
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-01-08 12:19:31 +0000

    :wrench: add astro rollup option

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import { remarkModifiedTime } from './src/lib/markdownit-last-modified';
 import mdx from '@astrojs/mdx';
+import polyfillNode from "rollup-plugin-polyfill-node";
+
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    }
+  },
   outDir: '_site',
   site: 'https://nabeelvalley.co.za/',
   markdown: {
