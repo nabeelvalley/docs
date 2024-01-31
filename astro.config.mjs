282758b434e0a4ab28d0e9b800c998a49e55af66
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-01-31 10:24:41 +0000

    :wrench: update astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import { remarkModifiedTime } from './src/lib/markdownit-last-modified';
 import mdx from '@astrojs/mdx';
 import polyfillNode from "rollup-plugin-polyfill-node";
 
-
+// https://astro.build/config
 export default defineConfig({
   outDir: '_site',
   site: 'https://nabeelvalley.co.za/',
   markdown: {
     remarkPlugins: [remarkModifiedTime],
     shikiConfig: {
-      theme: 'dark-plus',
-    },
+      theme: 'dark-plus'
+    }
   },
   integrations: [mdx({
     extendMarkdownConfig: true
-  }), sitemap()],
-})
+  }), sitemap()]
+});
