1bdf0edbc7b1e1ff518aab6020930b403a0f7578
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-03-21 06:33:31 +0100

    :construction: library page

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,13 +7,24 @@ const schema = z.object({
   published: z.boolean().optional(),
 })
 
+const bookSchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  date: z.string(),
+  isbn: z.string(),
+  published: z.number(),
+  status: z.enum(['complete', 'in-progress', 'abandoned']),
+})
+
 export const collections = {
   photography: defineCollection({ type: 'content', schema }),
   random: defineCollection({ type: 'content', schema }),
 
   'blog-md': defineCollection({ type: 'content', schema }),
-  'blog-ipynb': defineCollection({ type: "data", schema }),
+  'blog-ipynb': defineCollection({ type: 'data', schema }),
 
   'docs-md': defineCollection({ type: 'content', schema }),
-  'docs-ipynb': defineCollection({ type: "data", schema }),
+  'docs-ipynb': defineCollection({ type: 'data', schema }),
+
+  books: defineCollection({ type: 'data', schema: bookSchema }),
 }
