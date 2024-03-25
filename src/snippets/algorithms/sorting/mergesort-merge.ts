419678a0ad4ec4c83bf4eda031da89df9b9e637a
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-03-25 10:31:10 +0100

    :bug: fix mergesort implementation

--- src/snippets/algorithms/sorting/mergesort-merge.ts
+++ src/snippets/algorithms/sorting/mergesort-merge.ts
@@ -1,34 +1,4 @@
-import { Compare, Comparison } from './definition'
-
-export const isSortedRange = <T>(
-  compare: Compare<T>,
-  array: Array<T>,
-  s: number,
-  e: number
-) => {
-  const range = e - s
-
-  if (range < 1) {
-    console.error({ s, e, range })
-    throw new Error('Invalid Array Given')
-  }
-
-  if (range < 2) {
-    return true
-  }
-
-  for (let index = s; index < e; index++) {
-    const v = array[index]
-    const w = array[index + 1]
-
-    const result = compare(v, w)
-    if (result === Comparison.Greater) {
-      return false
-    }
-  }
-
-  return true
-}
+import { type Compare, Comparison } from './definition'
 
 /**
  * Merges the data in the range `lo` to `hi` into `a`. Assumes that the two
@@ -40,34 +10,24 @@ export const isSortedRange = <T>(
  */
 export const merge = <T>(
   compare: Compare<T>,
-  a: Array<T>,
+  array: Array<T>,
   aux: Array<T>,
   lo: number,
   mid: number,
   hi: number
 ) => {
-  // Assert that the input arrays are sorted
-  console.assert(
-    isSortedRange(compare, a, lo, mid),
-    'First half of array not sorted',
-    { a, lo, mid, hi }
-  )
-  console.assert(
-    isSortedRange(compare, a, mid + 1, hi),
-    'Second half of array not sorted',
-    { a, lo, mid, hi }
-  )
+  const isLessThan = (a: T, b: T) => compare(a, b) === Comparison.Less
 
   // Copy items into the auxillary array
-  for (let k = lo; k <= hi; k++) aux[k] = a[k]
+  for (let i = lo; i <= hi; i++) aux[i] = array[i]
 
-  let i = lo
-  let j = mid + 1
+  let l = lo
+  let h = mid + 1
 
-  for (let k = lo; k <= hi; k++) {
-    if (i > mid) a[k] = aux[j++]
-    else if (j > hi) a[k] = aux[i++]
-    else if (compare(aux[j], aux[i]) === Comparison.Less) a[k] = aux[j++]
-    else a[k] = aux[i++]
+  for (let i = lo; i <= hi; i++) {
+    if (l > mid) array[i] = aux[h++]
+    else if (h > hi) array[i] = aux[l++]
+    else if (isLessThan(aux[l], aux[h])) array[i] = aux[l++]
+    else array[i] = aux[h++]
   }
 }
--- src/snippets/algorithms/sorting/mergesort.ts
+++ src/snippets/algorithms/sorting/mergesort.ts
@@ -1,21 +1,25 @@
 import type { Compare } from './definition'
 import { merge } from './mergesort-merge'
 
-export const mergeSort = <T>(compare: Compare<T>, a: Array<T>): Array<T> => {
-  const aux = new Array<T>(a.length)
+export const mergeSort = <T>(
+  compare: Compare<T>,
+  array: Array<T>
+): Array<T> => {
+  const aux = new Array<T>(array.length)
 
   const sort = (lo: number, hi: number) => {
-    if (hi <= lo) {
+    if (hi - lo < 1) {
       return
     }
 
-    const mid = lo + (hi - lo) / 2
+    const mid = lo + Math.floor((hi - lo) / 2)
+
     sort(lo, mid)
     sort(mid + 1, hi)
-    merge(compare, a, aux, lo, mid, hi)
+    merge(compare, array, aux, lo, mid, hi)
   }
 
-  sort(0, a.length - 1)
+  sort(0, array.length - 1)
 
-  return a
+  return array
 }
