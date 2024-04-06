63ea0de4ad5dfdcbd28d0a94d2af8bb0bf5adbd3
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-04-06 19:54:31 +0200

    :lipstick: remove border from copy button

--- src/snippets/shaders/005_smoothstep.glsl
+++ src/snippets/shaders/005_smoothstep.glsl
@@ -9,7 +9,7 @@ uniform float u_time;
 void main(){
     vec2 st=gl_FragCoord.xy/u_resolution;
     
-    float y=smoothstep(.1,.9,st.x);
+    float y=smoothstep(.3,.7,st.x);
     vec3 color=vec3(y);
     
     gl_FragColor=vec4(color,1.);
