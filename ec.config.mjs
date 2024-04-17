1bd39096db2006c1bb139cfc223068f0ee43c3bc
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-04-17 13:01:11 +0200

    :memo: web animation slides in progress

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,18 +1,16 @@
-import  { defineEcConfig } from 'astro-expressive-code'
+import { defineEcConfig } from 'astro-expressive-code'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
 
-
-
 export default defineEcConfig({
-      themes: ['dracula-soft', 'rose-pine-dawn'],
-      plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
-      styleOverrides: {
-        codeBackground: 'var(--color-base)',
-        borderColor: 'var(--color-brand)',
+  themes: ['dracula-soft', 'rose-pine-dawn'],
+  plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
+  styleOverrides: {
+    codeBackground: 'var(--color-base)',
+    borderColor: 'var(--color-brand)',
     textMarkers: {
-        delDiffIndicatorContent: '',
-        insDiffIndicatorContent: ''
-    }
-      },
-    })
+      delDiffIndicatorContent: '',
+      insDiffIndicatorContent: '',
+    },
+  },
+})
