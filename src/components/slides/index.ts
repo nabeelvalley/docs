ea55757a6bf38710c80f7e2ce0dc8e9ed098d504
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-05-14 12:59:54 +0200

    :sparkles: profile slide

--- src/components/slides/index.ts
+++ src/components/slides/index.ts
@@ -1,3 +1,5 @@
 export { default as Slide } from './Slide.astro'
 export { default as Presentation } from './Presentation.astro'
 export { default as SlideOnly } from './SlideOnly.astro'
+
+export { default as About } from './About.astro'
