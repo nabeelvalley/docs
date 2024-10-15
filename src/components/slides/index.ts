b5273782d0e641640da4ae210afeeb1f21a146f3
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2024-10-15 21:04:56 +0200

    :sparkles: presenter notes

--- src/components/slides/index.ts
+++ src/components/slides/index.ts
@@ -1,5 +1,5 @@
 export { default as Slide } from './Slide.astro'
 export { default as Presentation } from './Presentation.astro'
 export { default as SlideOnly } from './SlideOnly.astro'
-
 export { default as About } from './About.astro'
+export { default as Note } from './Note.astro'
