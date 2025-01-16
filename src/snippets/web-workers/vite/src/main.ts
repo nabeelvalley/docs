2f8144cb210faa3d1b187e8038406e51330d180d
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2025-01-16 13:18:46 +0100

    :memo: add blocking function to web worker example

--- src/snippets/web-workers/vite/src/main.ts
+++ src/snippets/web-workers/vite/src/main.ts
@@ -1,18 +1,21 @@
-import CanvasWorker from './canvas.worker?worker'
-import type { InvokeParams } from './canvas.worker'
+import CanvasWorker from "./canvas.worker?worker";
+import type { InvokeParams } from "./canvas.worker";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const offScreenCanvas = document.createElement("canvas");
 
-const offScreenCanvas = document.createElement('canvas')
+app.appendChild(offScreenCanvas);
 
-app.appendChild(offScreenCanvas)
+const counter = document.createElement("p");
+app.appendChild(counter);
 
-const counter = document.createElement("p")
-app.appendChild(counter)
+const worker = new CanvasWorker();
 
-const worker = new CanvasWorker()
+const offscreen = offScreenCanvas.transferControlToOffscreen();
 
-const offscreen = offScreenCanvas.transferControlToOffscreen()
+worker.postMessage({ canvas: offscreen } satisfies InvokeParams, [offscreen]);
 
-worker.postMessage({ canvas: offscreen } satisfies InvokeParams, [offscreen])
+setInterval(() => {
+  alert("Block main thread now");
+}, 5000);
