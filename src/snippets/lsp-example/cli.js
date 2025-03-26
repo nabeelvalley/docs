d090607abd8d3682e6f22ca22b1c1224176473e5
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2025-03-26 15:09:13 +0100

    :construction: update example lsp snippet

--- src/snippets/lsp-example/cli.js
+++ src/snippets/lsp-example/cli.js
@@ -1,41 +1,43 @@
 #!/usr/bin/env node
 // @ts-check
 
-const { appendFileSync } = require("fs");
-const { stdout, stdin } = require("process");
+const { appendFileSync } = require("fs")
+const { stdout, stdin } = require("process")
 
 function log(contents) {
-  const logMessage =
-    typeof contents === "string" ? contents : JSON.stringify(contents);
-  return appendFileSync("./log", "\n" + logMessage + "\n");
+  const logMessage = typeof contents === 'string' ? contents : JSON.stringify(contents)
+  return appendFileSync('./log', "\n" + logMessage + "\n")
 }
+
+log("LSP Started")
 
 function parseMessage(message) {
-  log("Received");
-  log(message);
-  const parts = message.split("\r\n");
-  const body = JSON.parse(parts[parts.length - 1]);
+  log("Received")
+  log(message)
+  const parts = message.split('\r\n')
+  const body = JSON.parse(parts[parts.length - 1])
 
-  return body;
+  return body
 }
+
 
 function createResponse(message, result, error) {
   return {
     jsonrpc: "2.0",
     id: message.id,
     result,
-    error,
-  };
+    error
+  }
 }
 
-const METHOD_NOT_FOUND = -32601;
+const METHOD_NOT_FOUND = -32601
 
 function handleMessage(message) {
-  const isNotification = message.id === undefined;
+  const isNotification = message.id === undefined
 
   if (isNotification) {
     // Notifications don't require a response
-    return log(`Not responding to notification ${message.method}`);
+    return log(`Not responding to notification ${message.method}`)
   }
 
   switch (message.method) {
@@ -44,50 +46,50 @@ function handleMessage(message) {
         capabilities: {
           codeActionProvider: true,
           executeCommandProvider: {
-            commands: ["_nabeel.doSomethingCool"],
-          },
+            "commands": ["_example.doSomethingCool"]
+          }
         },
         serverInfo: {
-          name: "nabeel-lsp",
-          version: "0.0.1",
-        },
-      });
+          name: "example-lsp",
+          version: "0.0.1"
+        }
+      })
 
     case "textDocument/codeAction":
-      return createResponse(message, [
-        {
-          title: "Do something cool",
-          kind: "quickfix",
-          command: "_nabeel.doSomethingCool",
-        },
-      ]);
+      return createResponse(message, [{
+        title: "Do something cool",
+        kind: "quickfix",
+        command: "_example.doSomethingCool"
+      }])
 
     default:
       return createResponse(message, undefined, {
         code: METHOD_NOT_FOUND,
-        message: `Requested method not found in handler ${message.method}`,
-      });
+        message: `Requested method not found in handler ${message.method}`
+      })
   }
+
 }
 
 function sendResponse(result) {
-  const content = JSON.stringify(result);
-  const length = Buffer.byteLength(content, "utf-8");
+  const content = JSON.stringify(result)
+  const length = Buffer.byteLength(content, 'utf-8')
 
-  const response = `Content-Length: ${length}\r\n\r\n${content}`;
+  const response = `Content-Length: ${length}\r\n\r\n${content}`
 
-  log("Sending");
-  log(response);
-  stdout.write(response);
+  log("Sending")
+  log(response)
+  stdout.write(response)
 }
 
-stdin.on("data", (d) => {
-  const message = parseMessage(d.toString());
+stdin.on('data', (d) => {
+  const message = parseMessage(d.toString())
 
-  const response = handleMessage(message);
+  const response = handleMessage(message)
   if (response) {
-    sendResponse(response);
+    sendResponse(response)
   }
-});
+})
 
-process.on("exit", () => log("LSP process terminated"));
+
+process.on('exit', () => log('LSP process terminated'))
