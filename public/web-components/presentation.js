1c99b7cf74c8eec51b41db0ea89796d07c8e8d0a
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2025-07-23 23:12:09 +0200

    :art: organize presentation element so everyhing isn't one big functionion

--- public/web-components/presentation.js
+++ public/web-components/presentation.js
@@ -1,204 +1,203 @@
+const nextClass = 'presentation-next'
+const currClass = 'presentation-current'
+const prevClass = 'presentation-prev'
+
+const transitionClasses = [nextClass, currClass, prevClass]
+
 class Presentation extends HTMLElement {
   constructor() {
     super()
   }
 
+  listenersInitialized = false
+  slide = 0
+  slides = []
+
+  presenter = false
+  presenting = false
+
+  presentationId = window.location.href
+  syncWriter = createSyncWriter(this.presentationId)
+
   connectedCallback() {
-    const button = document.createElement('button')
-    button.id = 'presentation-button'
-    button.className = 'presentation-hidden'
-    button.type = 'button'
-    button.innerText = 'Start Presentation'
-    this.appendChild(button)
+    this.button = document.createElement('button')
+    this.button.id = 'presentation-button'
+    this.button.className = 'presentation-hidden'
+    this.button.type = 'button'
+    this.button.innerText = 'Start Presentation'
+    this.appendChild(this.button)
 
     const progress = document.createElement('div')
     progress.className = 'presentation-progress'
     this.appendChild(progress)
 
-    this.setupPresentation(button)
-  }
-
-  setupPresentation(button) {
-    let slides = Array.from(
+    this.slides = Array.from(
       document.querySelectorAll('site-presentation-slide'),
     )
 
-    let slide = 0
-    let presenter = false
-
-    const presentationId = window.location.href
-    const syncWriter = createSyncWriter(presentationId)
-
-    const nextSlide = () => {
-      if (slide === slides.length - 1) {
-        return slide
-      }
-
-      return slide + 1
-    }
-
-    const prevSlide = () => {
-      if (slide === 0) {
-        return slide
-      }
-
-      return slide - 1
-    }
-
-    const nextClass = 'presentation-next'
-    const currClass = 'presentation-current'
-    const prevClass = 'presentation-prev'
-
-    const transitionClasses = [nextClass, currClass, prevClass]
-
-    const keyHandlers = {
-      ArrowRight: nextSlide,
-      ArrowLeft: prevSlide,
-    }
-
-    const displaySlides = () => {
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('active', 'inactive', ...transitionClasses)
-
-        if (i === slide) {
-          slides[i].classList.add('active', currClass)
-        } else {
-          slides[i].classList.add('inactive')
-
-          if (i > slide) {
-            slides[i].classList.add(nextClass)
-          } else {
-            slides[i].classList.add(prevClass)
-          }
-        }
-      }
-    }
-
-    let presenting = false
-    const startPresentation = () => {
-      button.innerHTML = 'Resume presentation'
-      document.body.classList.add('presentation-overflow-hidden')
-
-      presenting = true
-      displaySlides()
-      setProgress()
-      initListeners()
-    }
-
-    const endPresentation = () => {
-      document.body.classList.remove('presentation-overflow-hidden')
-
-      presenting = false
-      slides.map((s) =>
-        s.classList.remove('active', 'inactive', ...transitionClasses),
-      )
-    }
-
-    const setPresenter = () => {
-      presenter = true
-      document.body.classList.add('presentation-presenter')
-      syncWriter(slide)
-    }
-
-    const setProgress = () => {
-      const progress = ((slide + 1) / slides.length) * 100
-      document.body.style.setProperty('--presentation-progress', `${progress}%`)
-    }
-
-    const transition = (nextSlide) => {
-      if (!presenting) {
-        return
-      }
-
-      if (slide === nextSlide) {
-        return
-      }
-
-      slides.forEach((s) => s.classList.remove(...transitionClasses))
-
-      if (presenter) {
-        syncWriter(nextSlide)
-      }
-
-      slide = nextSlide
-
-      displaySlides()
-      setProgress()
-    }
-
-    let listenersInitialized = false
-    const initListeners = () => {
-      if (listenersInitialized) {
-        return
-      }
-
-      listenersInitialized = true
-      window.addEventListener('keyup', (ev) => {
-        ev.preventDefault()
-        const isEscape = ev.key === 'Escape'
-        if (isEscape) {
-          endPresentation()
-          return
-        }
-
-        const isSpace = ev.key === ' '
-        if (isSpace) {
-          setPresenter()
-          return
-        }
-
-        const getSlide = keyHandlers[ev.key]
-
-        if (!getSlide) {
-          return
-        }
-
-        const nextSlide = getSlide()
-        transition(nextSlide)
-      })
-
-      let touchstartX = 0
-      let touchendX = 0
-      const handleGesure = () => {
-        const magnitude = Math.abs(touchstartX - touchendX)
-
-        if (magnitude < 40) {
-          // Ignore since this could be a scroll up/down
-          return
-        }
-
-        if (touchendX < touchstartX) {
-          transition(nextSlide())
-        }
-        if (touchendX > touchstartX) {
-          transition(prevSlide())
-        }
-      }
-
-      document.addEventListener(
-        'touchstart',
-        (ev) => {
-          touchstartX = ev.changedTouches[0].screenX
-        },
-        false,
-      )
-
-      document.addEventListener(
-        'touchend',
-        (event) => {
-          touchendX = event.changedTouches[0].screenX
-          handleGesure()
-        },
-        false,
-      )
-    }
-
     // If there is no presentation on the page then we don't initialize
-    if (slides.length) {
-      button.classList.remove('presentation-hidden')
-      button.addEventListener('click', startPresentation)
-      createSyncReader(presentationId, slide, transition)
+    if (this.slides.length) {
+      this.button.classList.remove('presentation-hidden')
+      this.button.addEventListener('click', this.startPresentation)
+      createSyncReader(this.presentationId, this.slide, this.transition)
     }
+  }
+
+  displaySlides = () => {
+    const slides = this.slides
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('active', 'inactive', ...transitionClasses)
+
+      if (i === this.slide) {
+        slides[i].classList.add('active', currClass)
+      } else {
+        slides[i].classList.add('inactive')
+
+        if (i > this.slide) {
+          slides[i].classList.add(nextClass)
+        } else {
+          slides[i].classList.add(prevClass)
+        }
+      }
+    }
+  }
+
+  nextSlide = () => {
+    if (this.slide === this.slides.length - 1) {
+      return this.slide
+    }
+
+    return this.slide + 1
+  }
+
+  prevSlide = () => {
+    if (this.slide === 0) {
+      return this.slide
+    }
+
+    return this.slide - 1
+  }
+
+  startPresentation = () => {
+    this.button.innerHTML = 'Resume presentation'
+    document.body.classList.add('presentation-overflow-hidden')
+
+    this.presenting = true
+    this.displaySlides()
+    this.setProgress()
+    this.initListeners()
+  }
+
+  endPresentation = () => {
+    document.body.classList.remove('presentation-overflow-hidden')
+
+    this.presenting = false
+    this.slides.map((s) =>
+      s.classList.remove('active', 'inactive', ...transitionClasses),
+    )
+  }
+
+  setProgress = () => {
+    const progress = ((this.slide + 1) / this.slides.length) * 100
+    document.body.style.setProperty('--presentation-progress', `${progress}%`)
+  }
+
+  transition = (nextSlide) => {
+    if (!this.presenting) {
+      return
+    }
+
+    if (this.slide === this.nextSlide) {
+      return
+    }
+
+    this.slides.forEach((s) => s.classList.remove(...transitionClasses))
+
+    if (this.presenter) {
+      this.syncWriter(nextSlide)
+    }
+
+    this.slide = nextSlide
+
+    this.displaySlides()
+    this.setProgress()
+  }
+
+  setPresenter = () => {
+    this.presenter = true
+    document.body.classList.add('presentation-presenter')
+    this.syncWriter(this.slide)
+  }
+
+  initListeners = () => {
+    const keyHandlers = {
+      ArrowRight: this.nextSlide,
+      ArrowLeft: this.prevSlide,
+    }
+
+    if (this.listenersInitialized) {
+      return
+    }
+
+    this.listenersInitialized = true
+    window.addEventListener('keyup', (ev) => {
+      ev.preventDefault()
+      const isEscape = ev.key === 'Escape'
+      if (isEscape) {
+        this.endPresentation()
+        return
+      }
+
+      const isSpace = ev.key === ' '
+      if (isSpace) {
+        this.setPresenter()
+        return
+      }
+
+      const getSlide = keyHandlers[ev.key]
+
+      if (!getSlide) {
+        return
+      }
+
+      const nextSlide = getSlide()
+      this.transition(nextSlide)
+    })
+
+    let touchstartX = 0
+    let touchendX = 0
+    const handleGesure = () => {
+      const magnitude = Math.abs(touchstartX - touchendX)
+
+      if (magnitude < 40) {
+        // Ignore since this could be a scroll up/down
+        return
+      }
+
+      if (touchendX < touchstartX) {
+        transition(nextSlide())
+      }
+      if (touchendX > touchstartX) {
+        transition(prevSlide())
+      }
+    }
+
+    document.addEventListener(
+      'touchstart',
+      (ev) => {
+        touchstartX = ev.changedTouches[0].screenX
+      },
+      false,
+    )
+
+    document.addEventListener(
+      'touchend',
+      (event) => {
+        touchendX = event.changedTouches[0].screenX
+        handleGesure()
+      },
+      false,
+    )
   }
 }
 
