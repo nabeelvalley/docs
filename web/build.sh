f92c70abeddd780daa70fff8ae338afacc17f677
Nabeel Valley <36758308+nabeelvalley@users.noreply.github.com>
2026-06-22 23:03:11 +0200

    :construction_worker: try alternative arch

--- web/build.sh
+++ web/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-wget https://github.com/gleam-lang/gleam/releases/download/v1.14.0/gleam-v1.14.0-aarch64-unknown-linux-musl.tar.gz
-wget https://github.com/gleam-lang/gleam/releases/download/v1.14.0/gleam-v1.14.0-aarch64-unknown-linux-musl.tar.gz.sha256
+wget https://github.com/gleam-lang/gleam/releases/download/v1.14.0/gleam-v1.14.0-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/gleam-lang/gleam/releases/download/v1.14.0/gleam-v1.14.0-x86_64-unknown-linux-musl.tar.gz.sha256
 
 ls -ll
 
 sha256sum -c gleam-v1.14.0-x86_64-unknown-linux-musl.tar.gz.sha256
 
-tar -xf gleam-v1.14.0-aarch64-unknown-linux-musl.tar.gz
+tar -xf gleam-v1.14.0-x86_64-unknown-linux-musl.tar.gz
 
 chmod +x gleam
 mv gleam ~/.local/bin/
